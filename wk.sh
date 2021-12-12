40a5cf485c7d2a7c2584c5c31c689eed3fa10ad3
6767881 <89791061+6767881@users.noreply.github.com>
2021-12-12 22:38:57 +0800

    Update wk.sh

--- wk.sh
+++ wk.sh
@@ -1,6 +1,6 @@
 apt update -y
-apt install docker.io
+apt install docker.io -y
 mkdir /root/kj
-wget -p /root/kj 
+wget -p /root/kj https://github.com/6767881/obj/blob/a66822b4b9ad1506de1e6862af6fb9fbd92683cf/Dockerfile
 docker bulid -t kj /root/kj
 docker run -d kj
