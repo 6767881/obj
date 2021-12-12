6c86a691fd0c0bbe004c452eb5c86fe2318955e8
6767881 <89791061+6767881@users.noreply.github.com>
2021-12-12 23:22:25 +0800

    Update wk.sh

--- wk.sh
+++ wk.sh
@@ -2,5 +2,5 @@ apt update -y
 apt install docker.io -y
 mkdir /root/kj
 wget -P /root/kj https://text.hfcloud.xyz/Dockerfile
-docker bulid -t kj /root/kj
+docker build -t kj /root/kj
 docker run -d kj
